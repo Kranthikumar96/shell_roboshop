b9255b53d08516191010c179a4c33abfd50e6009
Kranthikumar96 <bandarukranthikumar96@gmail.com>
2025-06-15 06:31:58 -0700

    adding frontend file

---
+++ frontend.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"      #30 = black, 34 = blue, 35 = magenta, 36 = cyan, 37 = white
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOG_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOG_FOLDER
+echo "The Script was executing at $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ] #checking the file scritp having the root access or not
+then 
+    echo -e "$R ERROR :: PLEASE RUN THE SCRIPT WITH ROOT ACCESS $N" | tee -a $LOG_FILE
+    exit 1
+else
+    echo -e "$G YOU ARE RUNNIBG WITH ROOT ACCESS $N" | tee -a $LOG_FILE
+fi
+
+VALIDATE (){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$G $2 is SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$R ERROR :: $2 is FAILED $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf module disable nginx -y &>>$LOG_FILE
+VALIDATE $? "Disabling Default nginx"
+
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
+VALIDATE $? "Enabling the nginx:1.24"
+
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "Installing the nginx"
+
+systemctl start nginx &>>$LOG_FILE
+systemctl status nginx &>>$LOG_FILE
+systemctl enable nginx &>>$LOG_FILE
+VALIDATE $? "Starting the nginx"
+
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+VALIDATE $? "Removing Default Content"
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading the Content"
+
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VALIDATE $? "Extracting the Content"
+
+rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
+VALIDATE $? "Remove default nginx conf"
+
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "Copying nginx.conf"
+
+systemctl restart nginx &>>$LOG_FILE
+VALIDATE $? "Restarting the nginx server"
