879dd6c0a56c1ed115dab47907ab135e98535fde
Kranthikumar96 <bandarukranthikumar96@gmail.com>
2025-06-15 23:37:01 -0700

    adding Mysql file

---
+++ mysql.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"      #30 = black, 34 = blue, 35 = magenta, 36 = cyan, 37 = white
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOG_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOG_FOLDER
+echo "The Script was executing at $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ] #checking the file scritp having the root access or not
+then 
+    echo -e "$R ERROR :: PLEASE RUN THE SCRIPT WITH ROOT ACCESS $N" | tee -a $LOG_FILE
+    exit 1
+else
+    echo -e "$G YOU ARE RUNNIBG WITH ROOT ACCESS $N" | tee -a $LOG_FILE
+fi
+
+echo "Please enter root password to setup RoboShop@1"
+read -s MYSQL_ROOT_PASSWORD
+
+VALIDATE (){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$G $2 is SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$R ERROR :: $2 is FAILED $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "Installing MySQL server"
+
+systemctl enable mysqld &>>$LOG_FILE
+VALIDATE $? "Enabling MySQL"
+
+systemctl start mysqld &>>$LOG_FILE
+VALIDATE $? "Starting MySQL"
+
+mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD &>>$LOG_FILE
+VALIDATE $? "Setting MySQL root password"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
