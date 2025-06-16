7e02c5ba3d10b7618c409c2433a7b4ad89244000
Kranthikumar96 <bandarukranthikumar96@gmail.com>
2025-06-15 22:36:07 -0700

    adding redis file

---
+++ redis.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"      #30 = black, 34 = blue, 35 = magenta, 36 = cyan, 37 = white
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOG_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOG_FOLDER
+echo "The Script was executing at $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ] #checking the file scritp having the root access or not
+then 
+    echo -e "$R ERROR :: PLEASE RUN THE SCRIPT WITH ROOT ACCESS $N" | tee -a $LOG_FILE
+    exit 1
+else
+    echo -e "$G YOU ARE RUNNIBG WITH ROOT ACCESS $N" | tee -a $LOG_FILE
+fi
+
+VALIDATE (){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$G $2 is SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$R ERROR :: $2 is FAILED $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf module disable redis -y &>>$LOG_FILE
+VALIDATE $? "Disabling Default Redis version"
+
+dnf module enable redis:7 -y &>>$LOG_FILE
+VALIDATE $? "Enabling redis:7"
+
+dnf install redis -y &>>$LOG_FILE
+VALIDATE $? "Installing redis:7"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Edited redis.conf to accept remote connections"
+
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enabling Redis"
+
+systemctl start redis  &>>$LOG_FILE
+VALIDATE $? "Started Redis"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
