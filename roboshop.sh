77648f1bdf56388dab1e8f2c3d50757489f425a2
Kranthikumar96 <bandarukranthikumar96@gmail.com>
2025-11-14 23:43:47 -0800

    shell

--- roboshop.sh
+++ roboshop.sh
@@ -10,7 +10,7 @@ DOMAIN_NAME="kranthicart.fun"
 #for instance in ${INSTANCES[@]} 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-000899abc99a0f48e --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0572e2c4a2faec51e --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
